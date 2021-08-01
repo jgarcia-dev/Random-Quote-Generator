268f2932189e50273a4a5cccac3b99c6d85a1f7f
John Garcia <johngarciadev@gmail.com>
2021-08-01 16:37:24 -0700

    Added new tags property to quotes and updated printQuote function to display them

--- js/script.js
+++ js/script.js
@@ -12,43 +12,49 @@ const quotes = [
     quote: "To choose doubt as a philosophy of life is akin to choosing immobility as a means of transportation.",
     source: "Yann Martel",
     citation: "Life of Pi",
-    year: 2001
+    year: 2001,
+    tags: ["Novel", "Adventure fiction", "Inspiration"]
   },
   {
     quote: "Enthusiasm makes up for a host of deficiencies.",
     source: "Barack Obama",
     citation: "A Promised Land",
-    year: 2020
+    year: 2020,
+    tags: ["Politics", "Inspiration"]
   },
   {
     quote: "Perseverance is not a long race; it is many short races one after the other.",
     source: "Walter Elliot",
     citation: "",
-    year: null
+    year: null,
+    tags: []
   },
   {
     quote: "No one can make you feel inferior without your consent.",
     source: "Eleanor Roosevelt",
     citation: "",
-    year: null
+    year: null,
+    tags: ["Politics", "Inspiration"]
   },
   {
     quote: "Courage is resistance to fear, mastery of fear, not absence of fear.",
     source: "Mark Twain",
     citation: "",
-    year: null
+    year: null,
+    tags: ["Inspiration"]
   },
   {
     quote: "Some people can read War and Peace and come away thinking it's a simple adventure story. Others can read the ingredients on a chewing gum wrapper and unlock the secrets of the universe.",
     source: "Lex Luthor",
     citation: "Superman",
-    year: 1987
+    year: 1987,
+    tags: ["Movies", "Fiction", "Villains"]
   }
 ]
 
 
 /***
- * returns a random quote from quotes array based on a random number
+ * returns a random quote object from quotes array based on a random number
 ***/
 function getRandomQuote() {
   const max = quotes.length;
@@ -63,6 +69,7 @@ function getRandomQuote() {
 function printQuote() {
   
   const randomQuote = getRandomQuote();
+  const numOfTags = randomQuote.tags.length;
 
   let HTML = `<p class="quote"> ${randomQuote.quote} </p>
   <p class="source"> ${randomQuote.source} `;
@@ -73,8 +80,15 @@ function printQuote() {
   if (randomQuote.year) {
     HTML += `<span class="year"> ${randomQuote.year} </span>`;
   }
-
   HTML += "</p>";
+
+  if (numOfTags !== 0) {
+    HTML += `<p class="tags">`
+    for (let i = 0; i < numOfTags; i++) {
+      HTML += `<span class="tag"> ${randomQuote.tags[i]} </span>`;
+    }
+    HTML += "</p>"
+  }
 
   document.getElementById('quote-box').innerHTML = HTML;
 }
